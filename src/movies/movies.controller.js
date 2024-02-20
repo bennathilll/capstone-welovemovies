013065460bc48dda112a3a053afeada9597838f4
Benjamin Hill <benjaminh@indeed.com>
2024-02-20 14:31:27 -0800

    Final welovemovies

---
+++ src/movies/movies.controller.js
@@ -0,0 +1,26 @@
+const service = require("./movies.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+async function movieExists(request, response, next) {
+  const movie = await service.read(request.params.movieId);
+  if (movie) {
+    response.locals.movie = movie;
+    return next();
+  }
+  next({ status: 404, message: "Movie cannot be found." });
+}
+
+async function read(request, response) {
+  const data = response.locals.movie;
+  response.json({ data: data });
+}
+
+async function list(request, response) {
+  const data = await service.list(request.query.is_showing);
+  response.json({ data });
+}
+
+module.exports = {
+  list: [asyncErrorBoundary(list)],
+  read: [asyncErrorBoundary(movieExists), read],
+};
---
+++ src/theaters/theaters.controller.js
@@ -0,0 +1,15 @@
+const service = require("./theaters.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+async function list(request, response) {
+  if (request.params.movieId) {
+    const data = await service.listByMovieId(request.params.movieId);
+    response.json({ data });
+  }
+  const data = await service.list();
+  response.json({ data });
+}
+
+module.exports = {
+  list: asyncErrorBoundary(list),
+};
